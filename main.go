a7d802b6eb424b294cf2b47cce07c48a774ff0db
baka <dyatlovleonid@gmail.com>
2021-09-08 21:56:48 +0300

    changes

--- main.go
+++ main.go
@@ -2,36 +2,55 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/taekwonzeus/botbuilder"
 )
 
-func main() {
-	session, err := botbuilder.BotData{
-		Token:  "",
-		Prefix: "z!",
-		Commands: []botbuilder.Command{{
-			Name:        "zalgo",
-			Description: "Turn your text into zalgo text",
-			Aliases:     []string{"z"},
-			Execute: func(session *discordgo.Session, event *discordgo.MessageCreate, args []string) {
+var token = ""
 
-			},
-		}},
-	}.Build()
+func main() {
+	session, err := discordgo.New("Bot " + token)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	session.AddHandler(onMessage)
+
 	session.Open()
+	defer session.Close()
 
-	var msg string
+	_, err = fmt.Scanln()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
 
-	for msg != "close" {
-		fmt.Scanln(&msg)
+func onMessage(session *discordgo.Session, event *discordgo.MessageCreate) {
+	tokens := strings.Fields(event.Content)
+	if len(tokens) < 1 {
+		return
 	}
 
-	session.Close()
+	if tokens[0] != "z!" {
+		return
+	}
+
+	if len(tokens) < 2 {
+		session.ChannelMessageSend(event.ChannelID, "Argument expected")
+		return
+	}
+
+	if len(tokens[1]) > 500 {
+		session.ChannelMessageSend(event.ChannelID, "Too many characters, max amount is 500")
+		return
+	}
+
+	session.ChannelMessageSendReply(event.ChannelID, toZalgo(tokens[1]), event.Reference())
+}
+
+func toZalgo(in string) string {
+	return ""
 }
